387e505fe7d982b0578109ad50ecc3a2310b4054
unknown <nanzheng03@gmail.com>
2020-06-20 11:20:44 +0800

    Fulfilled basic tasks and check

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ while game.winner is None:
     start, end = game.prompt()
     game.update(start, end)
     game.next_turn()
-print(f'Game over. {game.winner} player wins!')
+print(f"Game over. {game.winner} player wins!")
